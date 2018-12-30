56f4fb5e8a0a146d62897858231a5314ffb8208a
yughurt1984 <arron1984@gmail.com>
2018-12-30 11:02:09 +0800

    mac

--- 小甲鱼课堂/036/课后练习0.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# coding:utf-8
-
-class Person:
-    name = '小甲鱼'
-    
-    def printName(self):
-        print(self.name)
--- 小甲鱼课堂/test.py
+++
@@ -1,3 +0,0 @@
-import sys
-print(sys.path)
-print('Git 测试')
