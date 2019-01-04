e1c605d283b70eed6b3f8e78404c6d1e91da60ec
yughurt1984 <arron1984@gmail.com>
2019-01-04 17:20:02 +0800

    乌龟吃鱼

--- 小甲鱼课堂/037/课后练习1.py
+++ 小甲鱼课堂/037/课后练习1.py
@@ -3,7 +3,7 @@
 
 import random
 import easygui as g
-import numpy
+
 
 time = 0
 direction = ([-1,0], [0,-1], [1,0], [0,1])
@@ -11,48 +11,58 @@ direction = ([-1,0], [0,-1], [1,0], [0,1])
 
 class Turtle:
     def __init__(self, turtleLife):
-        self._turtlePosion = numpy.array([0, 0])
+        self._turtlePosion = [0, 0]
         self._turtleLife = turtleLife
     
     def move(self):
         self._life -= 1
-        moveDirection = numpy.array(random.choice(direction))        #随机一个方向
-        moveTimes = random.randint(1,3)                 #随机（1,2）次数
-        moveNum = 0
-        while moveNum != moveTimes:
+        turtleMoveDirection = random.choice(direction)       #随机一个方向
+        turtleMoveTimes = random.randint(1,3)                 #随机（1,2）次数
+        turtleMoveNum = 0
+        while turtleMoveNum != turtleMoveTimes:
             if 0 < self._turtlePosion[0] < 10 or 0 < self._turtlePosion[1] < 10:        #判断边际
-                self._turtlePosion = self._turtlePosion + moveDirection self._turtlePosion = self._posion + moveDirection             #移动位置
-                moveNum += 1
+                self._turtlePosion= [self._turtlePosion[0] + turtleMoveDirection[0], self._turtlePosion[1] + turtleMoveDirection[1]]     #移动位置       
+                turtleMoveNum += 1
             else:
-                continue
-        
+                continue  
     
-    def getLife(self):
-        return self._life
+    def getTurtleLife(self):
+        return self._turtleLife
 
-    def getPosion(self):  
-        return self._posion
+    def getTurtlePosion(self):  
+        return self._turtlePosion
         
        
 
 class Fish:
     def __init__(self, fishNum):
-        self._posionX = positionX
-        self._posionY = positionY
-        self._posion = [self._posionX, self._posionY]
-        self._life = life
+        self._fishNum = fishNum
     
     def move(self):
-        self._life -= 1
-        moveDirection = numpy.array(random.choice(direction))        #随机一个方向
-        moveTimes = random.randint(1,3)                         #随机（1,2）次数
-        moveNum = 0
-        while moveNum != moveTimes:
-            if 0 < self._turtlePosion[0] < 10 or 0 < self._turtlePosion[1] < 10:        #判断边际
-                self._turtlePosion = self._turtlePosion + moveDirection self._turtlePosion = self._posion + moveDirection             #移动位置
-                moveNum += 1
-            else:
-                continue
+        for i in range(0,self.fishNum):
+            self._fishPosionX = random.randint(0,10)
+            self._fishPosionY = random.randint(0,10)
+            self._fishPosion = [self._fishPosionX, self._fishPosionY]
+            self._fishPositon_list.append((i, self._fishPosion))
+        
+        fishMoveDirection = random.choice(direction)       #随机一个方向
+        for j in range(0, len(self._fishPositon_list)):      #10条鱼一起动
+            while fishMoveNum != fishMoveTimes:
+                if 0 < self._fishPositon_list[j][1][0] < 10 or 0 < self._fishPosion[j][1][1] < 10:        #判断边际
+                    self._fishPosion = [self._fishPositon_list[j][1][0] + fishMoveDirection[0], self._fishPositon_list[j][1][1] + fishMoveDirection[1]]
+                else:
+                    continue
 
-while self._life != 0 or self._fishMum != 0:
+    def getFishLife(self):
+        return self._fishLife
+
+    def getFishPosion(self):  
+        return self._fishPosion_list
+
+
+turtle = Turtle(100)
+fish = Fish(10)
+
+while self._turtleLife != 0 or self._fishMum != 0:
+
 
